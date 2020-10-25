91c16809a4c94e508bd18e05af901139d428758d
Alexander <51765464+alechries@users.noreply.github.com>
2020-10-25 18:09:27 +0300

    Update connection.py
    Add a function for secure queries to the database

--- connection.py
+++ connection.py
@@ -1,14 +1,27 @@
 from aiogram import Bot, Dispatcher
-from config import TELEGRAM_TOKEN, DATABASE_PATH
 import sqlite3 as sql
+from config import TELEGRAM_TOKEN, DATABASE_PATH
 
 bot = Bot(token=TELEGRAM_TOKEN)
 dispatcher = Dispatcher(bot)
 
-sqlConnect = sql.connect(DATABASE_PATH, check_same_thread=False)
-sqlCursor = sqlConnect.cursor()
-sqlCursor.execute('''CREATE TABLE IF NOT EXISTS USERS(
-    user_id INTEGER,
-    first_name VARCHAR,
-    message_id INT,
-    message VARCHAR)''')
+
+def database_try_create():
+    database_query('''CREATE TABLE IF NOT EXISTS messages(
+        user_id INTEGER,
+        first_name VARCHAR,
+        message_id INT,
+        message VARCHAR)''')
+
+
+def database_query(query: str):
+    connect = sql.connect(DATABASE_PATH, check_same_thread=False)
+    with connect:
+        cursor = connect.cursor()
+        cursor.execute(query)
+        result = cursor.fetchall()
+    if connect:
+        connect.commit()
+        connect.close()
+
+    return result
