2d0da2e501ba14af67f61a771bea55922392614c
Alexander <51765464+alechries@users.noreply.github.com>
2020-10-25 17:10:38 +0200

    Add files via upload

---
+++ feedback.py
@@ -0,0 +1,42 @@
+from aiogram import types
+import logging as log
+from config import FEEDBACK_USER_ID
+from connection import bot, database_query
+
+
+async def feedback_message(message: types.Message):
+    log.info(f'Message ({message.content_type}) from {message.from_user.first_name} ({message.from_user.id}) with text: {message.text}')
+
+    try:
+        if message.from_user.id == FEEDBACK_USER_ID:
+            if message.reply_to_message is None:
+                await message.answer(f'To reply to the user, reply to the message sent from him')
+            else:
+                units = database_query(f"SELECT user_id FROM messages WHERE message_id = {message.reply_to_message.message_id}")
+                unit = units[0][0]
+                if message.content_type == "text":
+                    await bot.send_message(unit, message.text)
+                elif message.content_type == "photo":
+                    await bot.send_photo(unit, message.photo[-1].file_id)
+                elif message.content_type == "video":
+                    await bot.send_video(unit, message.video.file_id)
+                elif message.content_type == "sticker":
+                    await bot.send_sticker(unit, message.sticker.file_id)
+                elif message.content_type == "audio":
+                    await bot.send_audio(unit, message.audio.file_id)
+                elif message.content_type == "voice":
+                    await bot.send_voice(unit, message.voice.file_id)
+                elif message.content_type == "document":
+                    await bot.send_document(unit, message.document.file_id)
+                elif message.content_type == "location":
+                    await bot.send_location(unit, message.location)
+                elif message.content_type == "animation":
+                    await bot.send_animation(unit, message.animation.file_id)
+                elif message.content_type == "contact":
+                    await bot.send_contact(unit, message.contact.file_id)
+        else:
+            forward_message_result = await bot.forward_message(FEEDBACK_USER_ID, message.chat.id, message.message_id)
+            database_query(f"INSERT OR IGNORE INTO messages(user_id,first_name,message_id,message) "
+                           f"VALUES('{message.from_user.id}','{message.from_user.first_name}','{forward_message_result.message_id}','{message.text}')")
+    except Exception as e:
+        log.exception(e)
