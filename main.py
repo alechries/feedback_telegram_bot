457145745c87105c309ccd73405f863172a29f22
Alexander <51765464+alechries@users.noreply.github.com>
2020-11-05 15:36:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,33 @@
-import asyncio
-from aiogram import types
+from aiogram import types, executor
 import logging as log
 from feedback import feedback_message
-from connection import dispatcher, database_try_create
+from connection import dispatcher, database
 
 
-async def main():
-    log.info("Bot started")
-    database_try_create()
-    await dispatcher.start_polling()
+def database_try_create():
+    database.execute(
+        '''
+        CREATE TABLE 
+        IF NOT EXISTS messages(
+        user_id INTEGER,
+        first_name VARCHAR,
+        message_id INT,
+        message VARCHAR)
+        '''
+    )
 
 
-@dispatcher.message_handler(commands=['start'])
+@dispatcher.message_handler(commands=["start"])
 async def welcome_message_event(message: types.Message):
     await message.answer(f"Hello, {message.from_user.first_name}!")
 
     
-@dp.message_handler(content_types=types.ContentType.ANY)
+@dispatcher.message_handler(content_types=types.ContentType.ANY)
 async def feedback_message_event(message: types.Message):
     await feedback_message(message)
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    log.info("Bot started")
+    database_try_create()
+    executor.start_polling(dispatcher)
